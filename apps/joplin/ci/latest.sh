a468745a848f2d38d630e3ba6440285f804ca9fc
JJGadgets <git@jjgadgets.tech>
2023-12-05 17:32:29 +0800

    feat: add joplin distroless

---
+++ apps/joplin/ci/latest.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+channel=$1
+
+if [[ "${channel}" == "beta" ]]; then
+    version="$(curl -sX GET "https://api.github.com/repos/laurent22/joplin/releases" | jq --raw-output 'map(select(.prerelease)) | first | .tag_name' 2>/dev/null)"
+fi
+
+if [[ "${channel}" == "stable" ]]; then
+    version="$(curl -vX GET "https://hub.docker.com/v2/repositories/joplin/server/tags/?page_size=
+1" | jq --raw-output '.results.[0].name')"
+fi
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
