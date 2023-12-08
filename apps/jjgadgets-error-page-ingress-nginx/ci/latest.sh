45fa0a9a57eba96d55c4516aa22c6279133b09d5
JJGadgets <git@jjgadgets.tech>
2023-12-09 03:04:43 +0800

    fix(error-page): extra brackets

--- apps/jjgadgets-error-page-ingress-nginx/ci/latest.sh
+++ apps/jjgadgets-error-page-ingress-nginx/ci/latest.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-version="1.0.0-caddy-2.7.5"
+version="1.0.0-caddy-2.7.5" # TODO: figure out appending version tags while versioning my actual containers
 printf "%s" "${version}"
