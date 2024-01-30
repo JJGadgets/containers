b891ba10c7d59f592d71f35f20c03930e4a4e414
JJGadgets <git@jjgadgets.tech>
2024-01-30 20:43:00 +0800

    feat: add findmydeviceserver

---
+++ apps/findmydeviceserver/ci/latest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+channel=$1
+
+if [[ "${channel}" == "master" ]]; then
+    version="$(date +%Y%m%d)"
+fi
+
+if [[ "${channel}" == "stable" ]]; then
+    version="$(curl -sX GET "https://gitlab.com/api/v4/projects/Nulide%2Ffindmydeviceserver/releases" | jq -r '.[0].tag_name' 2>/dev/null)"
+fi
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
