4e4fb67e751570a06257ea83561b6ab9c76cf25c
JJGadgets <git@jjgadgets.tech>
2024-01-30 20:54:50 +0800

    fix(findmydeviceserver): master version

--- apps/findmydeviceserver/ci/latest.sh
+++ apps/findmydeviceserver/ci/latest.sh
@@ -8,6 +8,6 @@ fi
 if [[ "${channel}" == "stable" ]]; then
     version="$(curl -sX GET "https://gitlab.com/api/v4/projects/Nulide%2Ffindmydeviceserver/releases" | jq -r '.[0].tag_name' 2>/dev/null)"
 fi
-version="${version#*v}"
+#version="${version#*v}" # because of master channel, it's easier to leave the v in
 version="${version#*release-}"
 printf "%s" "${version}"
