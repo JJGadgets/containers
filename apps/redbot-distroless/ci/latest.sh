dc97bf0f0f21b8ed6225a516d5e1a9a31fa26e48
JJGadgets <git@jjgadgets.tech>
2024-02-04 15:24:04 +0800

    fix(redbot): split images

---
+++ apps/redbot-distroless/ci/latest.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+channel=$1
+
+if [[ "${channel}" == "stable" ]]; then
+    version="$(curl -sX GET "https://api.github.com/repos/Cog-Creators/Red-DiscordBot/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+fi
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
