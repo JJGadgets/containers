4541d7dd3b4c465cea52a98ff8aff42cc26fad2e
JJGadgets <git@jjgadgets.tech>
2024-03-16 06:51:24 +0800

    fix(phanpy): rm hash removal in versioning

--- apps/phanpy/ci/latest.sh
+++ apps/phanpy/ci/latest.sh
@@ -2,7 +2,7 @@
 channel=$1
 
 if [[ "${channel}" == "stable" ]]; then
-    version="$(curl -sX GET "https://api.github.com/repos/cheeaun/phanpy/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null | awk -F'.' 'sub(FS $NF,x)')" # https://unix.stackexchange.com/a/549210
+    version="$(curl -sX GET "https://api.github.com/repos/cheeaun/phanpy/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
 fi
 version="${version#*v}"
 version="${version#*release-}"
