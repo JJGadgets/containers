2929b6deac7584bfe94a91b44ebc3b53474c59bb
JJGadgets <git@jjgadgets.tech>
2024-07-17 20:14:48 +0800

    feat: add Radicale with LDAP plugin

---
+++ apps/radicale/ci/latest.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+channel=$1
+
+if [[ "${channel}" == "beta" ]]; then
+    version="$(curl -sX GET "https://api.github.com/repos/Kozea/Radicale/releases" | jq --raw-output 'map(select(.prerelease)) | first | .tag_name' 2>/dev/null)"
+fi
+
+if [[ "${channel}" == "stable" ]]; then
+    version="$(curl -sX GET "https://api.github.com/repos/Kozea/Radicale/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+fi
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
