271ef02847cecd718f66a357ab7ffb1688cefd92
JJGadgets <git@jjgadgets.tech>
2024-07-18 09:30:43 +0800

    fix(radicale): revert tmp workaround

--- apps/radicale/ci/latest.sh
+++ apps/radicale/ci/latest.sh
@@ -2,12 +2,12 @@
 channel=$1
 
 if [[ "${channel}" == "beta" ]]; then
-    version="$(curl -sX GET "https://api.github.com/repos/Kozea/Radicale/releases" | jq --raw-output 'map(select(.prerelease)) | first | .tag_name' 2>/dev/null)"
+    #version="$(curl -sX GET "https://api.github.com/repos/Kozea/Radicale/releases" | jq --raw-output 'map(select(.prerelease)) | first | .tag_name' 2>/dev/null)"
+    version="3.2.2"
 fi
 
 if [[ "${channel}" == "stable" ]]; then
-    #version="$(curl -sX GET "https://api.github.com/repos/Kozea/Radicale/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
-    version="3.2.2"
+    version="$(curl -sX GET "https://api.github.com/repos/Kozea/Radicale/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
 fi
 version="${version#*v}"
 version="${version#*release-}"
