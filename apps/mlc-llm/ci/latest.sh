42f8cd994c02e55dbc3ec0e2a9ab2e48c26cb6e4
JJGadgets <git@jjgadgets.tech>
2024-10-07 14:36:45 +0800

    feat(mlc-llm): stable and nightly variants

--- apps/mlc-llm/ci/latest.sh
+++ apps/mlc-llm/ci/latest.sh
@@ -6,7 +6,7 @@ channel=$1
 if [[ "${channel}" == "stable" ]]; then
     version="$(curl -s https://mlc.ai/wheels | grep -oE '>mlc_llm_cpu-(.*)-cp311-manylinux.*_x86_64.whl' | awk -F'-' '{print $2}' | tail -n 1)"
 elif [[ "${channel}" == "nightly" ]]; then
-    version="$(date +%Y%m%d)"
+    version="$(date +%Y.%m.%d)"
 else
     echo 'No version could be extracted!'
     exit 1
