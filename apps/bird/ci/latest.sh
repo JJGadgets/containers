ca5f398aaf97c17eaf2f178209f118808a3ff3ad
JJGadgets <git@jjgadgets.tech>
2024-10-13 03:55:39 +0800

    chore: cleanup

--- apps/bird/ci/latest.sh
+++ apps/bird/ci/latest.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #version="$(curl -sX GET "https://gitlab.nic.cz/labs/bird/-/raw/master/NEWS" 2>/dev/null | awk '/Version/{print $2}' 2>/dev/null | head -n 1 2>/dev/null)"
-version="$(curl -sX GET "https://git.alpinelinux.org/aports/plain/community/bird/APKBUILD" | awk -F'=' '/pkgver/{print $2}')"
+version="$(curl -sX GET "https://git.alpinelinux.org/aports/plain/community/bird/APKBUILD?h=3.20-stable" | awk -F'=' '/pkgver/{print $2}')"
 version="${version#*v}"
 version="${version#*release-}"
-version="${version}-r$(curl -sX GET "https://git.alpinelinux.org/aports/plain/community/bird/APKBUILD" | awk -F'=' '/pkgrel/{print $2}')"
+version="${version}-r$(curl -sX GET "https://git.alpinelinux.org/aports/plain/community/bird/APKBUILD?h=3.20-stable" | awk -F'=' '/pkgrel/{print $2}')"
 printf "%s" "${version}"
