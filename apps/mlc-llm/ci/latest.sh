5d13544fa28da2fd1d9ff26f76de350745c717c9
JJGadgets <git@jjgadgets.tech>
2024-10-13 03:57:35 +0800

    chore: cleanup

--- apps/mlc-llm/ci/latest.sh
+++ apps/mlc-llm/ci/latest.sh
@@ -5,7 +5,11 @@ channel=$1
 
 if [[ "${channel}" == "stable" ]]; then
     version="$(curl -s https://mlc.ai/wheels | grep -oE '>mlc_llm_cpu-(.*)-cp311-manylinux.*_x86_64.whl' | awk -F'-' '{print $2}' | tail -n 1)"
+elif [[ "${channel}" == "cuda" ]]; then
+    version="$(curl -s https://mlc.ai/wheels | grep -oE '>mlc_llm_cpu-(.*)-cp311-manylinux.*_x86_64.whl' | awk -F'-' '{print $2}' | tail -n 1)"
 elif [[ "${channel}" == "nightly" ]]; then
+    version="$(date +%Y.%m.%d)"
+elif [[ "${channel}" == "cuda-nightly" ]]; then
     version="$(date +%Y.%m.%d)"
 else
     echo 'No version could be extracted!'
