b5b1a4a0f5273cd74efed7c22f5da59527389134
JJGadgets <git@jjgadgets.tech>
2026-03-23 01:30:03 +0800

    feat: add forticertinetes

---
+++ apps/forticertinetes/ci/latest.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+channel=$1
+
+# if [[ "${channel}" == "stable" ]]; then
+#     version="$(curl -sX GET "https://api.github.com/repos/JJGadgets/FortiDynaSync/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
+# fi
+version="rolling"
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
---
+++ apps/forticertinetes/forticertinetes.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# FortiCertinetes
+# © 2026 JJGadgets (https://jjgadgets.tech) (https://social.jjgadgets.tech/@jj) (https://github.com/JJGadgets)
+# Licensed under GNU AGPL v3.0.0
+# NOT AFFILIATED WITH OR AN OFFICIAL PRODUCT/SERVICE BY FORTINET.
+
+export FGT_HOST="${FGT_HOST:=192.168.1.99}"
+export FGT_PORT="${FGT_PORT:=22}"
+export FGT_USER="${FGT_USER:=admin}"
+export CLUSTER="${CLUSTER:=k8s}"
+
+if [ -z "${FGT_VDOM}" ]; then
+  FGT_CERT_CONF="config vpn certificate local"
+elif [ "${FGT_VDOM}" = "global" ]; then
+  FGT_CERT_CONF="config global
+  config certificate local"
+elif [ "${FGT_VDOM}" != "global" ]; then
+  FGT_CERT_CONF="config vdom
+  edit ${FGT_VDOM}
+  config vpn certificate local"
+fi
+echo "$FGT_CERT_CONF"
+
+kubectl get certificates -A -o jsonpath='{range .items[*]}{.metadata.namespace}{"|"}{.spec.secretName}{"\n"}{end}' | while IFS='|' read -r NAMESPACE SECRET_NAME
+do
+  PRIVATE_KEY="$(kubectl get secrets -n "${NAMESPACE}" "${SECRET_NAME}" -o jsonpath='{.data.tls\.key}' | base64 -d)"
+  CERTIFICATE="$(kubectl get secrets -n "${NAMESPACE}" "${SECRET_NAME}" -o jsonpath='{.data.tls\.crt}' | base64 -d)"
+  CERT_NAME="$(printf '%s' "${CLUSTER}_${NAMESPACE}_${SECRET_NAME}" | cut -c1-35)"
+  echo
+  echo "${NAMESPACE}"
+  echo "${SECRET_NAME}"
+  echo "${CERT_NAME}"
+  echo
+  ssh "${FGT_USER}"@"${FGT_HOST}" -p "${FGT_PORT}" << EOF
+    ${FGT_CERT_CONF}
+      edit "${CERT_NAME}"
+        set private-key "${PRIVATE_KEY}"
+        set certificate "${CERTIFICATE}"
+      end
+    end
+EOF
+done
