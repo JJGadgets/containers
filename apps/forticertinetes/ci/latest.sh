f599a372059707d4498023268c68484ad363f456
JJGadgets <git@jjgadgets.tech>
2026-05-04 06:21:09 +0800

    fix(forticertinetes): wrong user for apk add

--- apps/forticertinetes/ci/latest.sh
+++ apps/forticertinetes/ci/latest.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-channel=$1
-
-# if [[ "${channel}" == "stable" ]]; then
-#     version="$(curl -sX GET "https://api.github.com/repos/JJGadgets/FortiDynaSync/releases/latest" | jq --raw-output '.tag_name' 2>/dev/null)"
-# fi
 version="rolling"
 version="${version#*v}"
 version="${version#*release-}"
